837bbbd54ae8124921486e5101efa1b87621d36e
nitin singh <nitinsinghrahtore223@gmail.com>
2025-02-14 15:39:30 +0530

    Basic pattern

---
+++ OneDrive/Desktop/if else/pattern/Pattern8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pattern8{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of row ");
+        int row = sc.nextInt();
+
+        for(int i=1;i<=row;i++){
+            for(int s=row-1;s>=i;s--){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
