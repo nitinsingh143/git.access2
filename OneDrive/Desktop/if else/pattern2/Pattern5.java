318ba40ce835683cda1c4645e71e82701dd614da
nitin singh <nitinsinghrahtore223@gmail.com>
2025-02-14 16:15:08 +0530

    Advance Pattern

---
+++ OneDrive/Desktop/if else/pattern2/Pattern5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the value");
+        int row =sc.nextInt();
+        int x=1;
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=row;j++){
+                System.out.print(x+" ");
+                x+=2;
+            }
+            System.out.println();
+        }
+    }
+}
